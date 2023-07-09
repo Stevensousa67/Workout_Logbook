3316145404ca7e6a7ad84705d29778036fe9fb96
gc04346 <gc04346@gmail.com>
2023-07-09 19:57:00 -0300

    add cors config

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'nested_admin',
     'rest_framework',
     'django_filters',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +51,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_HEADERS = [
+    'Authorization',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
